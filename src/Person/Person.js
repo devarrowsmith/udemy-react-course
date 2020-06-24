bdd5e6bb904391e9063c19fd94bce679ea5cf86e
GummiCode <gummicodeblog@gmail.com>
2020-06-24 02:35:41 +0100

    Added a simple JS function to the <p> element of the Person component using curly braces. {}.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 
+
 const Person = props => {
-  return <p>Hey! I'm a person!</p>
+  return <p>Hey! I'm a person! I'm {Math.floor((Math.random())*100)} years old!</p>
 };
 
 export default Person;
