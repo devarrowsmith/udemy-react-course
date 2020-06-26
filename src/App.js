08875d586a663a6deb45c7139e445fcd87cf08eb
GummiCode <gummicodeblog@gmail.com>
2020-06-26 23:25:47 +0100

    Replaced hard-coded parameters in App.js with State for App.js.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+
+//Here we define the component'S State, an Object containing parameters.
+//If anything in a component's State is changed while the app is rendered, React will update that component in the DOM; so it will update the page. 
+  state = {
+    persons: [
+      { name: 'David', age: 25 },
+      { name: 'Pablo', age: 28 },
+      { name: 'Dani', age: 26 }
+    ]
+  }
+
+  // Here we render the direct Child component(/s).
   render() {
     return (
       <div className="App">
        <h1>Hi! I'm a React app!</h1>
-       <Person name='David' age='25' />
-       <Person name='Pablo' age='27' />
-       <Person name='Dani' age='26' > This text is a child of a Perosn component. Children can be passed to a component using the reserved word 'children'. </Person>
+       <button>Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+         <p>This text is a child of a Perosn component. Children can be passed to a component using the reserved word 'children'.</p>
+         <p>You can pass in a ton of structured HTML using props.children.</p> 
+      </Person>
       </div>
     );
+
+    //NB: Any children of the child components (grandchildren of app.js, if you will), should be rendered within those children. A sensible heirarchy of components must be maintained so that the app's modules are composed in a sensible human-useable structure.
 
 
   /* the code above compiles to:
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -11,4 +11,8 @@ const Person = props => {
   );
 };
 
+//In the code above, props.children allows us to pass HTML elements into this component at the parent level.
+// When this component is called in the parent, we put this structured HTML in the 'person' element (component) we create in JSX.
+// eg. <Person> <h1>This is the material accessed by props.children.</h1></Person>
+
 export default Person;
